9cb0fc8635dc532d182a0c6d4a4079faa72fcb91
Benjamine <benjamineemanuel2017@gmail.com>
2026-02-19 05:12:04 +0100

    Adding constructors and getters

--- src/main/java/SchoolMgtSystem/Person.java
+++ src/main/java/SchoolMgtSystem/Person.java
@@ -1,4 +1,28 @@
 package SchoolMgtSystem;
 
-public class Person {
+//This class represent every human which is abstract
+public abstract class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    //Constructors
+    public Person(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+    //Getters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
